b7b5d81b2ffb3def25bfd844f924382243d464fb
smmsa1912 <lovecandidate@gmail.com>
2024-06-03 19:31:50 +0530

    working login/signup created

--- Auth/views.py
+++ Auth/views.py
@@ -6,35 +6,25 @@ from django.http import HttpResponse
 # Create your views here.
 def Signup(request):
     if request.method == "GET":
-        data = {"signup_form": SignupForm(), "login_form": LoginForm()}
+        data = {"signup_form": SignupForm(), "login_form": LoginForm(), "page": "login"}
         return render(request, "signup.html", data)
     else:
         if 'signup' in request.POST:
-            print("POST")
             form = SignupForm(request.POST)
             if form.is_valid():
-                print("Valid")
-                email = form.cleaned_data['Email']
-                password = form.cleaned_data['Password']
-                print(email, password)
                 form.save()
                 return HttpResponse("Signup successful")
             else:
                 print(form.errors)
-                return render(request, "signup.html", {"signup_form": form, "login_form": LoginForm()})
+                return render(request, "signup.html", {"signup_form": form, "login_form": LoginForm(), "page": "signup"})
 
         elif 'login' in request.POST:
-            print("POST LOGIN")
             form = LoginForm(request.POST)
             if form.is_valid():
-                print("Valid")
-                email = form.cleaned_data['Email']
-                password = form.cleaned_data['Password']
-                print(email, password)
                 return HttpResponse("Login successful")
             else:
                 print(form.errors)
-                return render(request, "signup.html", {"login_form": form, "signup_form": SignupForm()})
+                return render(request, "signup.html", {"login_form": form, "signup_form": SignupForm(), "page": "login"})
 
         else:
             print(request.POST)
--- static/JS/signup.js
+++ static/JS/signup.js
@@ -2,10 +2,18 @@ const container = document.getElementById('container');
 const registerBtn = document.getElementById('register');
 const loginBtn = document.getElementById('login');
 
+console.log(mode)
+
+if (mode === 'signup') {
+    container.classList.add("active");
+}
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
 });
 
+if (mode === 'login') {
+    container.classList.remove("active");
+}
 registerBtn.addEventListener('click', () => {
     container.classList.add("active");
 });
