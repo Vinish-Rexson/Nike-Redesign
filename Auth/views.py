9a66d5bdf10630b3087240df5c4b5606ec7cd991
smmsa1912 <lovecandidate@gmail.com>
2024-06-03 19:10:54 +0530

    working login/signup created

--- Auth/views.py
+++ Auth/views.py
@@ -10,19 +10,32 @@ def Signup(request):
         return render(request, "signup.html", data)
     else:
         if 'signup' in request.POST:
+            print("POST")
             form = SignupForm(request.POST)
             if form.is_valid():
+                print("Valid")
+                email = form.cleaned_data['Email']
+                password = form.cleaned_data['Password']
+                print(email, password)
                 form.save()
                 return HttpResponse("Signup successful")
             else:
+                print(form.errors)
                 return render(request, "signup.html", {"signup_form": form, "login_form": LoginForm()})
 
         elif 'login' in request.POST:
+            print("POST LOGIN")
             form = LoginForm(request.POST)
             if form.is_valid():
+                print("Valid")
+                email = form.cleaned_data['Email']
+                password = form.cleaned_data['Password']
+                print(email, password)
                 return HttpResponse("Login successful")
             else:
+                print(form.errors)
                 return render(request, "signup.html", {"login_form": form, "signup_form": SignupForm()})
 
         else:
+            print(request.POST)
             return HttpResponse("Invalid request")
